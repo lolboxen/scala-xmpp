d06a19d289c745febb7392f5593d10729f4ea614
Trent Ahrens <trent.ahrens@gmail.com>
2014-12-13 19:06:38 -0500

    fixed test

--- src/test/scala/com/lolboxen/xmpp/PacketReaderTest.scala
+++ src/test/scala/com/lolboxen/xmpp/PacketReaderTest.scala
@@ -5,12 +5,12 @@ import akka.testkit.{ImplicitSender, TestKit}
 import akka.util.ByteString
 import com.lolboxen.xmpp.packet.{StreamEnded, StreamBegan}
 import com.lolboxen.xmpp.packet.parsing.StreamParser
-import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+import org.scalatest._
 
 /**
  * Created by Trent Ahrens on 12/9/14.
  */
-class PacketReaderTest(_system: ActorSystem) extends TestKit(_system) with ImplicitSender with WordSpecLike with Matchers with BeforeAndAfterAll {
+class PacketReaderTest(_system: ActorSystem) extends TestKit(_system) with ImplicitSender with FlatSpecLike with Matchers with BeforeAndAfterAll {
   def this() = this(ActorSystem("MySpec"))
 
   val packetReader = system.actorOf(Props(classOf[PacketReader], testActor, List(new StreamParser())))
@@ -19,12 +19,10 @@ class PacketReaderTest(_system: ActorSystem) extends TestKit(_system) with Impli
     TestKit.shutdownActorSystem(system)
   }
 
-  it must {
-    "correct decode packets" in {
-      packetReader ! akka.io.Tcp.Received(ByteString("""<stream:stream xmlns="jabber:client" xmlns:stream="http://etherx.jabber.org/streams" id="1" version="1.0" from="domain.com"></stream:stream>"""))
-      expectMsgAllOf(
-        Received(StreamBegan(Some("1"), "1.0", Some(Jid("domain.com")), None)),
-        Received(StreamEnded))
-    }
+  it should "correct decode packets" in {
+    packetReader ! akka.io.Tcp.Received(ByteString("""<stream:stream xmlns="jabber:client" xmlns:stream="http://etherx.jabber.org/streams" id="1" version="1.0" from="domain.com"></stream:stream>"""))
+    expectMsgAllOf(
+      Received(StreamBegan(Some("1"), "1.0", Some(Jid("domain.com")), None)),
+      Received(StreamEnded))
   }
 }
